6c35723c5a040addad7eaeaaf602bcf7eb32010a
Martin Coronel <martincxx@gmail.com>
2023-03-25 22:21:15 +0300

    add two simple routes

--- hello/views.py
+++ hello/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def brian(request):
+    return HttpResponse("Hi, Brian")
+
+
+def david(request):
+    return HttpResponse("Hello, David!")
