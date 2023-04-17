7dd64a4f3aec8fd2a86ebf409852f4a80bd9aa04
Martin Coronel <martincxx@gmail.com>
2023-04-17 23:01:04 +0300

    Fix env conflict

--- lecture3/settings.py
+++ lecture3/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
