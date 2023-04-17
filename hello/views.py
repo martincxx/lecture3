2cee4ee3fa9aca0b1ccaa9eadb6f0110b807bdd5
Martin Coronel <martincxx@gmail.com>
2023-04-17 22:15:48 +0300

    Default route

--- hello/views.py
+++ hello/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 
 
 def brian(request):
