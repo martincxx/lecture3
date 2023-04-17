b5b934b1ec0954712b222eee945ac9b574e6020e
Martin Coronel <martincxx@gmail.com>
2023-04-17 21:31:20 +0300

    fix dependencies

--- lecture3/settings.py
+++ lecture3/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "hello",
+    "newyear",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
