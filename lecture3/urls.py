a70fae8af1c915472f81f034c878da5526fa09ae
Martin Coronel <martincxx@gmail.com>
2023-04-17 22:44:55 +0300

    Add new year app

--- lecture3/urls.py
+++ lecture3/urls.py
@@ -17,4 +17,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [path("admin/", admin.site.urls),
-               path("hello/", include("hello.urls"))]
+               path("hello/", include("hello.urls")),
+               path("newyear/", include("newyear.urls"))]
--- newyear/views.py
+++ newyear/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from datetime import datetime
 # Create your views here.
+
+
+def index(request):
+    now = datetime.now()
+    return render(request, "newyear/index.html", {
+        "date": now.day == 1 and now.month == 1
+    })
