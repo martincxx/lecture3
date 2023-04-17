8be4990234614f3ae14eea14caf28d4faa94868d
Martin Coronel <martincxx@gmail.com>
2023-04-17 22:12:14 +0300

    Initial urls

--- lecture3/urls.py
+++ lecture3/urls.py
@@ -16,4 +16,5 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("hello/", include("hello.urls"))]
+urlpatterns = [path("admin/", admin.site.urls),
+               path("hello/", include("hello.urls"))]
