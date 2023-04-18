1ff79cd34d0fdd5b6b560e749d0f0b7e851ae245
Martin Coronel <martincxx@gmail.com>
2023-04-18 23:06:41 +0300

    Implement add new tasks from form

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django import forms
+from django.urls import reverse
 # Create your views here.
-tasks = ["foo", "bar", "baz"]
+tasks = []
 
 
 class NewTaskForm(forms.Form):
@@ -15,6 +17,17 @@ def index(request):
 
 
 def add(request):
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
+    # render an empty form
     return render(request, "tasks/add.html", {
         "form": NewTaskForm()
     })
